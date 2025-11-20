82d569e21ad82179a2d641182d9dc7e58ce98f60
Jair <jairclo32@gmail.com>
2025-11-20 14:21:51 -0300

    Força bind no endereço 0.0.0.0 para corrigir erro 502

--- index.js
+++ index.js
@@ -93,6 +93,6 @@ app.get('/', (req, res) => {
 
 // --- INICIALIZAÇÃO DO SERVIDOR ---
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
